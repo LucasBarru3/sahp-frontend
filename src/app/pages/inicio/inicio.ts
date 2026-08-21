cf728521163cf1659a472b9bc8978cf031f50683
Rolk254 <rolk254@gmail.com>
2026-08-21 07:23:40 +0200

    Fix de translate

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 @Component({
   selector: 'app-inicio',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [
+    RouterModule,
+    TranslatePipe
+  ],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
-export class Inicio  {
+export class Inicio {
   classes: any[] = [];
   loading = false;
-  constructor() {}
 }
